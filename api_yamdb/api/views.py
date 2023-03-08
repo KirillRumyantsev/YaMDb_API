ed912624f938bc7cb50a0bf5f9903038c45e1d14
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-03-08 19:14:55 +0300

    Commit-25: Добавил filters.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,6 +3,7 @@ from django.db.models import Avg
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework import filters
@@ -13,7 +14,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import (Category, Comment, Genre, GenreTitle, Review,
                             Title, User)
 
-
+from .filters import TitleFilter
 from .permissions import (IsAuthorOrReadOnly, IsRoleAdmin, IsRoleModerator,
                           ReadOnly)
 from .serializers import (CategorySerializer, CommentSerializer,
@@ -108,11 +109,13 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = (
-        Title.objects.all()
+        Title.objects
         .annotate(rating=Avg('reviews__score'))
         .order_by('-id')
     )
     permission_classes = (IsRoleAdmin | ReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH',):
