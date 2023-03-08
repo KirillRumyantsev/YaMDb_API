df352f66a9d8ebcc3b0148c465130c503db23c95
Кирилл Румянцев <kirillsonytablet@gmail.com>
2023-03-08 23:32:47 +0300

    Commit-28: Для ревью

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,29 +1,22 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets, mixins
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework import filters
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
-from reviews.models import (Category, Comment, Genre, GenreTitle, Review,
-                            Title, User)
-
-from .filters import TitleFilter
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 from .filters import TitleFilter
 from .permissions import (IsAuthorOrReadOnly, IsRoleAdmin, IsRoleModerator,
                           ReadOnly)
-from .serializers import (CategorySerializer, CommentSerializer,
-                          GenreSerializer, ReviewSerializer,
-                          AuthentificationSerializer, TitleListSerializer,
-                          TitlePostSerializer, UserSerializer,
-                          TokenSerializer)
+from .serializers import (AuthentificationSerializer, CategorySerializer,
+                          CommentSerializer, GenreSerializer, ReviewSerializer,
+                          TitleListSerializer, TitlePostSerializer,
+                          TokenSerializer, UserSerializer)
 
 
 class CreateListDestroy(
